cce0ca1618487166097c17d5e3c9018480646de8
Karthik Sethuraman <bobrocks93@gmail.com>
2014-09-13 20:11:32 -0400

    timeSeriesPlot

--- timeSeriesPlot.R
+++ timeSeriesPlot.R
@@ -11,17 +11,30 @@ count <- 1
 
 while(length(line <- readLines(f,n=1)) > 0) {
   lineSplit <- strsplit(line, " ")[[1]]
+
   dataframe <- rbind(dataframe, as.numeric(lineSplit))
+  if(count == 1) {
+	names(dataframe) <- c("x1", "y1", "x2", "y2")
+  }
+
+  if(count %% 100 == 1){
+ 	plot(1, dataframe[count, 1]+1, col = "red", ylim = c(-2, 2), xlim = c(0, 201), ylab = "X", xlab = "Frame", type = "p", pch = 18)
+ 	abline(h=0)
+ 	abline(v=100.5)
+  	lines(1+101, dataframe[count, 2]+1, col = "green", type = "p", pch = 18)
+
+	lines(1, dataframe[count, 3]-1, col = "red", type = "p", pch = 18)
+	lines(1+101, dataframe[count, 4]-1, col = "green", type = "p", pch = 18)
+  } else {
+  	lines(count%%100, dataframe[count, 1]+1, col = "red", type = "p", pch = 18)
+  	lines(count%%100+101, dataframe[count, 2]+1, col = "green", type = "p", pch = 18)
+
+	lines(count%%100, dataframe[count, 3]-1, col = "red", type = "p", pch = 18)
+	lines(count%%100+101, dataframe[count, 4]-1, col = "green", type = "p", pch = 18)
+  }
+
+  count <- count+1
+
 }
-
-names(dataframe) <- c("x1", "y1", "x2", "y2")
-
-par(mfrow = c(2, 1))
-
-plot(1:dim(dataframe)[1], dataframe[, 1], col = "red", ylim = c(-1, 1), type = "l", ylab = "X", xlab = "Frame")
-lines(1:dim(dataframe)[1], dataframe[, 3], col = "green")
-
-plot(1:dim(dataframe)[1], dataframe[, 2], col = "red", ylim = c(-1, 1), type = "l", ylab = "Y", xlab = "Frame")
-lines(1:dim(dataframe)[1], dataframe[, 4], col = "green")
 
 Sys.sleep(5)
